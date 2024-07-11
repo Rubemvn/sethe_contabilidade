9b8951172991fe7b967a3a5ed9322c3fcb305787
Rúbem Vieira <rubemvn17@gmail.com>
2024-07-11 18:04:30 -0300

    feat: adicionar arquivos de temas

---
+++ styles/themes/ light.js
@@ -0,0 +1,25 @@
+import device from "../device";
+
+export default {
+	title: "light",
+	colors: {
+		primary: "#FFFFFF",
+		secondary: "#6DD1AE",
+		tertiary: "#073447",
+		quartiary: "#00161F",
+
+		theme01: "#ffffff",
+		theme02: "#ededed",
+		theme03: "#00161F",
+
+		theme04: "#6DD1AE",
+		theme05: "#FFFFFF",
+
+		white: "#ffffff",
+		black: "#000000",
+		medium_aquamarine: "#6DD1AE",
+		dark_slate_gray: "#073447",
+		rich_black: "#00161F",
+	},
+	device,
+};
---
+++ styles/themes/dark.js
@@ -0,0 +1,26 @@
+import device from "../device";
+
+export default {
+	title: "dark",
+	colors: {
+		primary: "#073447",
+		secondary: "#6DD1AE",
+		tertiary: "#FFFFFF",
+		quartiary: "#00161F",
+
+		theme01: "#00161F",
+		theme02: "#001C28",
+		theme03: "#FFFFFF",
+
+		theme04: "#FFFFFF",
+		theme05: "#6DD1AE",
+
+		white: "#ffffff",
+		black: "#000000",
+		medium_aquamarine: "#6DD1AE",
+		dark_slate_gray: "#073447",
+		rich_black: "#00161F",
+	},
+
+	device,
+};
